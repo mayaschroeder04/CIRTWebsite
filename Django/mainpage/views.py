2327d5a7f4cfcba02bda98733f1f3504929a7a96
Parker Malmgren <parker@themalmgrens.com>
2025-03-03 19:09:47 -0500

    updated frontend page

--- Django/mainpage/views.py
+++ Django/mainpage/views.py
@@ -3,3 +3,13 @@ from django.http import HttpResponse
 def home(request):
     return render(request, 'main.html')
 # Create your views here.
+def homepage(request):
+    return render(request, "homepage.html")
+
+ 
+def fake_journal(request):
+    return render(request, "fake_journal-2.html")
+
+
+def TOC(request):
+    return render(request, "terms-and-conditions.html")
