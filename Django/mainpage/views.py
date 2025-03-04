a8ce3cd019c23dfbd33c3a65d2eff2f752e68a42
Parker Malmgren <parker@themalmgrens.com>
2025-03-03 19:25:06 -0500

    updated files to combine all work

--- Django/mainpage/views.py
+++ Django/mainpage/views.py
@@ -1,12 +1,11 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-def home(request):
-    return render(request, 'main.html')
+
+
 # Create your views here.
 def homepage(request):
     return render(request, "homepage.html")
 
- 
+
 def fake_journal(request):
     return render(request, "fake_journal-2.html")
 
--- Web_Dev/storefront/settings.py
+++ Web_Dev/storefront/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-x`
 #
 # # Ensure `STATICFILES_DIRS` is correct
 # STATICFILES_DIRS = [
