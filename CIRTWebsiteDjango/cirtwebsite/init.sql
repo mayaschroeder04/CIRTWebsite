380d61d03c8ff7b42245ab89d3140b4f52f957ce
Parker Malmgren <parker@themalmgrens.com>
2025-03-05 17:35:06 -0500

    creates webpages table

--- CIRTWebsiteDjango/cirtwebsite/init.sql
+++ CIRTWebsiteDjango/cirtwebsite/init.sql
@@ -22,3 +22,14 @@ GRANT ALL PRIVILEGES ON criminology_db.* TO 'admin'@'%';
 
 -- Flush privileges to apply changes
 FLUSH PRIVILEGES;
+
+USE criminology_db;
+
+CREATE TABLE IF NOT EXISTS web_pages (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(255),
+    html_content TEXT
+);
+
+INSERT INTO web_pages (title, html_content)
+VALUES ('mainpage', '$(cat /docker-entrypoint-initdb.d/main.html)');
