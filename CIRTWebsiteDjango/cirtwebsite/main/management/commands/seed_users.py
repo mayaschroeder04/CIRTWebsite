7b0a99f954e1567c8a650dd2b75602e2ba08daf6
Parker Malmgren <parker@themalmgrens.com>
2025-04-02 09:22:39 -0400

    Added first and last name to the suers to display

--- CIRTWebsiteDjango/cirtwebsite/main/management/commands/seed_users.py
+++ CIRTWebsiteDjango/cirtwebsite/main/management/commands/seed_users.py
@@ -11,22 +11,22 @@ class Command(BaseCommand):
 
         # Check if superuser exists, if not, create one
         if not User.objects.filter(username='admin').exists():
-            user = User.objects.create_superuser('admin', 'parker@themalmgrens.com', 'adminpassword')
+            user = User.objects.create_superuser(username='admin',email='parker@themalmgrens.com',password='adminpassword', first_name='admin', last_name='admin')
             print(f"Superuser created: {user.username}")
         else:
             print("Superuser already exists")
 
         # Check if other users exist, if not, create them
         users = [
-            ('JohnDoe', 'johndoe@example.com', 'password123', '0'),
-            ('AliceDoe', 'janedoe@example.com', 'password456', '1'),
-            ('sayedali', 'bobdoe@example.com', 'password789', '0'),
-            ('abc', 'abc@example.com', 'abc', '0')
+            ('JohnDoe', 'johndoe@example.com', 'password123', '0', 'John', 'Doe'),
+            ('maya', 'janedoe@example.com', 'maya', '1', 'Alice', 'Doe'),
+            ('sayedali', 'bobdoe@example.com', 'password789', '0', 'Sayed', 'Ali'),
+            ('abc', 'abc@example.com', 'abc', '0', 'Adam', 'Cobb', )
         ]
 
-        for username, email, password, is_staff in users:
+        for username, email, password, is_staff, first_name, last_name in users:
             if not User.objects.filter(username=username).exists():  # Check if the user exists
-                user = User.objects.create_user(username=username, email=email)
+                user = User.objects.create_user(username=username, email=email, is_staff=is_staff, first_name=first_name, last_name=username)
                 user.set_password(password)  # Hash the password before saving
                 user.save()
                 print(f"User {username} created")
